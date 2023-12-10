6e948a6caea606d1c3af693e8fd38f4c08677fe3
Dzima-G <dgukalin@gmail.com>
2023-12-10 09:58:47 +0300

    added to the apartment page - list of owners

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class OwnerInline(admin.TabularInline):
+    model = Owner.owned_apartments.through
+    raw_id_fields = ('owner', 'flat',)
+
+
 class AuthorAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner_deprecated',)
     readonly_fields = ['created_at', ]
@@ -15,6 +20,7 @@ class AuthorAdmin(admin.ModelAdmin):
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony',)
     raw_id_fields = ('likes',)
+    inlines = [OwnerInline, ]
 
 
 admin.site.register(Flat, AuthorAdmin)

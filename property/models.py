59e1bf2672c63da3117136fc2a3113a41cc29636
Dzima-G <dgukalin@gmail.com>
2023-12-16 09:35:39 +0300

    fixed redundant data removed

--- property/models.py
+++ property/models.py
@@ -5,9 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner_deprecated = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
-    owner_pure_phone = PhoneNumberField('Нормализированный номер владельца', blank=True, null=True)
     new_building = models.BooleanField(null=True, blank=True)
 
     created_at = models.DateTimeField(
